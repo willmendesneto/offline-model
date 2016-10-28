9b2611e1cf6a3b6a930def077341c5916bbff2c5
Will Mendes <willmendesneto@gmail.com>
2016-10-28 22:14:16 +1100

    refactor(test): Changing test structure for OfflineStorage

--- test/offline-storage.spec.js
+++ test/offline-storage.spec.js
@@ -4,194 +4,129 @@ const expect = require('chai').expect;
 const OfflineStorage = require('../lib/offline-storage');
 const sinon = require('sinon');
 
-describe('Service: OfflineStorage', function () {
-
-  // instantiate service
-  var secret,
-    storageType,
-    key,
-    dataString,
-    sandbox;
-
-  beforeEach(function () {
-    sandbox = sinon.sandbox.create();
-    secret = 'secret';
-    key = 'key';
-    dataString = { testValue: 'test'};
-  });
-
-  describe('using "localStorage"', function(){
+['localStorage', 'sessionStorage'].forEach(function(webStorage) {
+  describe('Service: OfflineStorage', function () {
 
     beforeEach(function () {
-      storageType = 'localStorage';
+      this.sandbox = sinon.sandbox.create();
+      this.secret = 'secret';
+      this.key = 'key';
+      this.dataString = { testValue: 'test' };
     });
 
-    describe('init', function(){
-      it('service should be initialyzeds with internal variables configurateds', function () {
-        OfflineStorage.init({secret: secret, storageType: storageType});
+    describe('using "' + webStorage + '"', function(){
 
-        expect(OfflineStorage.JSON !== null).to.be.equal(true);
-        expect(OfflineStorage.storageType).to.be.equal(storageType);
-        expect(OfflineStorage.secret !== '').to.be.equal(true);
-      });
-    });
-
-    describe('encrypt', function(){
-      it('should returns a string with informations', function () {
-        expect(typeof OfflineStorage.encrypt(dataString) === 'string').to.be.equal(true);
-      });
-    });
-
-    describe('decrypt', function(){
-      it('should returns a json object decrypted', function () {
-        var mockedData = Object.assign({}, { data: dataString, expiry: null});
-        var decriptedData = OfflineStorage.decrypt(JSON.stringify(mockedData));
-        expect(decriptedData).to.have.property('testValue');
-        expect(decriptedData).to.not.have.property('expiry');
-        expect(decriptedData.testValue).to.be.equal(dataString.testValue);
-        expect(decriptedData.expiry).not.to.be.null;
+      beforeEach(function () {
+        this.storageType = webStorage;
       });
 
-      it('should return null if data is expired', function () {
-        var expiredData = { data: dataString };
-        expiredData.expiry = new Date();
-        expiredData.expiry -= 1000;
-        expiredData.expiry = new Date(expiredData.expiry);
-        var decriptedData = OfflineStorage.decrypt(JSON.stringify(expiredData));
-        expect(decriptedData).to.equal(null);
-      });
-    });
-
-    describe('set', function(){
-      it('should returns "true" when element is setted', function () {
-        expect(OfflineStorage.set(key, dataString)).to.be.equal(true);
-      });
-
-      it('should returns "false" when second param isn\'t setted', function () {
-        expect(OfflineStorage.set(key)).to.be.equal(false);
-      });
-    });
-
-    describe('get', function(){
-      it('should returns the original json object', function () {
-        OfflineStorage.set(key, dataString);
-        expect(OfflineStorage.get(key)).to.deep.equal(dataString);
-      });
-
-      describe('When a data is expired', function() {
+      describe('When the service is initialised', function(){
         beforeEach(function() {
-          sandbox.stub(window.localStorage, 'getItem').returns(JSON.stringify({
-            data: [],
-            expiry: (new Date(2010, 10, 10)).getTime()
-          }));
+          OfflineStorage.init({secret: this.secret, storageType: this.storageType});
         });
 
-        afterEach(function() {
-          sandbox.restore();
-        });
-
-        it('should return null if data is expired', function () {
-          expect(OfflineStorage.get(key)).to.equal(null);
+        it('should be configured with internal information', function () {
+          expect(OfflineStorage.storageType).to.be.equal(this.storageType);
+          expect(OfflineStorage.secret).to.be.equal(this.secret);
         });
       });
-    });
 
-    describe('remove', function(){
-      it('should returns "true" when element was removed with success', function () {
-        OfflineStorage.set(key, dataString);
-        expect(OfflineStorage.remove(key)).to.be.equal(true);
-      });
-    });
-  });
-
-  describe('using "sessionStorage"', function(){
-
-    beforeEach(function () {
-      storageType = 'sessionStorage';
-    });
-
-    describe('init', function(){
-      it('service should be initialyzeds with internal variables configurateds', function () {
-        OfflineStorage.init({secret: secret, storageType: storageType});
-
-        expect(OfflineStorage.JSON !== null).to.be.equal(true);
-        expect(OfflineStorage.storageType).to.be.equal(storageType);
-        expect(OfflineStorage.secret !== '').to.be.equal(true);
-      });
-    });
-
-    describe('encrypt', function(){
-      it('should returns a string with informations', function () {
-        expect(typeof OfflineStorage.encrypt(dataString) === 'string').to.be.equal(true);
-      });
-    });
-
-    describe('decrypt', function(){
-      it('should returns a json object decrypted', function () {
-        var mockedData = Object.assign({}, { data: dataString, expiry: null});
-        var decriptedData = OfflineStorage.decrypt(JSON.stringify(mockedData));
-        expect(decriptedData).to.have.property('testValue');
-        expect(decriptedData).to.not.have.property('expiry');
-        expect(decriptedData.testValue).to.be.equal(dataString.testValue);
-        expect(decriptedData.expiry).not.to.be.null;
+      describe('When the data is encrypted', function(){
+        it('should returns a string with informations', function () {
+          expect(typeof OfflineStorage.encrypt(this.dataString)).to.be.equal('string');
+        });
       });
 
-      it('should return null if data is expired', function () {
-        var expiredData = { data: dataString };
-        expiredData.expiry = new Date();
-        expiredData.expiry -= 1000;
-        expiredData.expiry = new Date(expiredData.expiry);
-        var decriptedData = OfflineStorage.decrypt(JSON.stringify(expiredData));
-        expect(decriptedData).to.equal(null);
-      });
-    });
-
-    describe('set', function(){
-      it('should returns "true" when element is setted', function () {
-        expect(OfflineStorage.set(key, dataString)).to.be.equal(true);
-      });
-
-      it('should returns "false" when second param isn\'t setted', function () {
-        expect(OfflineStorage.set(key)).to.be.equal(false);
-      });
-    });
-
-    describe('get', function(){
-      it('should returns the original json object', function () {
-        OfflineStorage.set(key, dataString);
-        expect(OfflineStorage.get(key)).to.deep.equal(dataString);
-      });
-
-      describe('When a data is expired', function() {
+      describe('When the data is decrypted', function(){
         beforeEach(function() {
-          sandbox.stub(window.sessionStorage, 'getItem').returns(JSON.stringify({
-            data: [],
-            expiry: (new Date(2010, 10, 10)).getTime()
-          }));
+          const mockedData = Object.assign({}, { data: this.dataString, expiry: null});
+          this.decriptedData = OfflineStorage.decrypt(JSON.stringify(mockedData));
         });
 
-        afterEach(function() {
-          sandbox.restore();
+        it('should returns a json object decrypted', function () {
+          expect(this.decriptedData).to.have.property('testValue');
+          expect(this.decriptedData).to.not.have.property('expiry');
+          expect(this.decriptedData.testValue).to.be.equal(this.dataString.testValue);
+          expect(this.decriptedData.expiry).not.to.be.null;
         });
 
-        it('should return null if data is expired', function () {
-          expect(OfflineStorage.get(key)).to.equal(null);
+        describe('When the data is expired', function() {
+          beforeEach(function() {
+            let expiredData = { data: this.dataString };
+            expiredData.expiry = new Date();
+            expiredData.expiry -= 1000;
+            expiredData.expiry = new Date(expiredData.expiry);
+            this.decriptedData = OfflineStorage.decrypt(JSON.stringify(expiredData));
+          });
+
+          it('should return null if data is expired', function () {
+            expect(this.decriptedData).to.equal(null);
+          });
         });
       });
-    });
 
-    describe('remove', function(){
-      it('should returns "true" when element was removed with success', function () {
-        OfflineStorage.set(key, dataString);
-        expect(OfflineStorage.remove(key)).to.be.equal(true);
+      describe('When a data is added', function(){
+        it('should returns "true" when element is setted', function () {
+          expect(OfflineStorage.set(this.key, this.dataString)).to.be.equal(true);
+        });
+
+        it('should returns "false" when second param isn\'t setted', function () {
+          expect(OfflineStorage.set(this.key)).to.be.equal(false);
+        });
+
+        describe('When a data is expired', function() {
+          beforeEach(function() {
+            this.webStorageSpy = this.sandbox.spy(window[webStorage], 'setItem');
+            this.encriptedDataMock = JSON.stringify({
+              data: this.dataString,
+              expiry: 10
+            });
+            this.sandbox.stub(OfflineStorage, 'encrypt').returns(this.encriptedDataMock);
+            OfflineStorage.set(this.key, this.dataString, 10);
+          });
+
+          afterEach(function() {
+            this.sandbox.restore();
+          });
+
+          it('should add expiry information in ' + webStorage, function() {
+            expect(this.webStorageSpy.calledWith(this.key, this.encriptedDataMock)).to.equal(true);
+          });
+        });
       });
-    });
 
-    describe('clearAll', function(){
-      it('should remove all items with success', function () {
-        OfflineStorage.set(key, dataString);
-        expect(OfflineStorage.clearAll()).to.be.equal(true);
-        expect(OfflineStorage.get(key)).to.be.equal(null);
+      describe('When an added data is required', function(){
+        beforeEach(function() {
+          OfflineStorage.set(this.key, this.dataString);
+        });
+
+        it('should returns the original json object', function () {
+          expect(OfflineStorage.get(this.key)).to.deep.equal(this.dataString);
+        });
+
+        describe('When a data is expired', function() {
+          beforeEach(function() {
+            this.sandbox.stub(window[webStorage], 'getItem').returns(JSON.stringify({
+              data: [],
+              expiry: (new Date(2010, 10, 10)).getTime()
+            }));
+          });
+
+          afterEach(function() {
+            this.sandbox.restore();
+          });
+
+          it('should return null', function () {
+            expect(OfflineStorage.get(this.key)).to.equal(null);
+          });
+        });
+      });
+
+      describe('When a data is removed', function(){
+        it('should returns "true" if the element was removed with success', function () {
+          OfflineStorage.set(this.key, this.dataString);
+          expect(OfflineStorage.remove(this.key)).to.be.equal(true);
+        });
       });
     });
   });
